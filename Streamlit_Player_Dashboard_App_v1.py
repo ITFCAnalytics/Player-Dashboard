c2f909dd2eaae0140b69f84094ff422948a203d2
ITFCAnalytics <82184672+ITFCAnalytics@users.noreply.github.com>
2025-05-22 21:50:09 +0100

    Add files via upload

--- Streamlit_Player_Dashboard_App_v1.py
+++ Streamlit_Player_Dashboard_App_v1.py
@@ -31,7 +31,7 @@ root = os.getcwd() + '/'
 
 url = 'https://www.dropbox.com/scl/fi/nsqh1o4lywfs5it9buu88/Final-FBRef-All-Leagues.csv?rlkey=jhci52hjq0lw8xjnyhydng3uj&raw=1'
 st.write(url)
-df = pd.read_csv(url, delimiter=",")
+df = pd.read_csv(url, engine='python', error_bad_lines=False)
 
 #https://drive.google.com/uc?id=1AZhc9HS0IBH0FMC7NArwRP3eu6n14Bv2&export=download
 
