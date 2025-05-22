1178659d9d9ebc7fd24ab0ca2a75f862fee9b659
ITFCAnalytics <82184672+ITFCAnalytics@users.noreply.github.com>
2025-05-22 21:56:47 +0100

    Add files via upload

--- Streamlit_Player_Dashboard_App_v1.py
+++ Streamlit_Player_Dashboard_App_v1.py
@@ -29,7 +29,8 @@ import matplotlib
 
 root = os.getcwd() + '/'
 
-url = 'https://www.dropbox.com/scl/fi/nsqh1o4lywfs5it9buu88/Final-FBRef-All-Leagues.csv?rlkey=jhci52hjq0lw8xjnyhydng3uj&raw=1'
+url = 'https://drive.google.com/uc?export=download&id=1AZhc9HS0IBH0FMC7NArwRP3eu6n14Bv2'
+#url = 'https://www.dropbox.com/scl/fi/nsqh1o4lywfs5it9buu88/Final-FBRef-All-Leagues.csv?rlkey=jhci52hjq0lw8xjnyhydng3uj&raw=1'
 st.write(url)
 
 df = pd.read_csv(url, engine='python', on_bad_lines='skip')
